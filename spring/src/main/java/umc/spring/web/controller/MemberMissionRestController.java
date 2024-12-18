42dc0da91db45807c4c02d8497dcc324e0bded1b
LJYeon12 <juyeon010530@gmail.com>
2024-12-18 20:32:58 +0900

    change branch

--- spring/src/main/java/umc/spring/web/controller/MemberMissionRestController.java
+++ spring/src/main/java/umc/spring/web/controller/MemberMissionRestController.java
@@ -40,21 +40,28 @@ public class MemberMissionRestController {
     @Operation(summary = "진행 중인 미션 목록 조회 API",description = "진행 중인 미션들의 목록을 조회하는 API이며, 페이징을 포함합니다. query String 으로 page 번호를 주세요")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access 토큰 만료",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access 토큰 모양이 이상함",
+                    content = @Content(schema = @Schema(implementation = ApiResponse.class))),
     })
     @Parameters({
             @Parameter(name = "memberId", description = "멤버 Id, path variable 입니다!!")
     })
-    public ApiResponse<MemberMissionResponseDTO.progressMissionListDTO> getProgressMissionList(@PathVariable("memberId") Long memberId,@RequestParam(name = "page") Integer page) {
+    public ApiResponse<MemberMissionResponseDTO.progressMissionListDTO> getProgressMissionList(
+            @PathVariable("memberId") Long memberId,
+            @RequestParam(name = "page") Integer page)
+    {
         Page<Mission> progressMissionList = memberMissionQueryService.getProgressMissionList(memberId, page);
         return ApiResponse.onSuccess(MemberMissionConverter.progressMissionListDTO(progressMissionList));
     }
 
     @PatchMapping("/to-completed/{missionId}")
     @Operation(summary = "진행 중인 미션 완료 미션으로 변환 API",description = "진행 중인 미션을 완료 미션으로 변환하는 API 이다.")
-    public ApiResponse<MemberMissionResponseDTO.changeToCompletedMissionDTO> patchChangeToCompletedMissionDTO(@PathVariable("missionId") Long missionId, @PathVariable("memberId") Long memberId) {
+    public ApiResponse<MemberMissionResponseDTO.changeToCompletedMissionDTO> patchChangeToCompletedMissionDTO(
+            @PathVariable("missionId") Long missionId, @PathVariable("memberId") Long memberId) {
         MemberMission memberMission = memberMissionCommandService.patchToCompletedMissionDTO(memberId, missionId);
         return ApiResponse.onSuccess(MemberMissionConverter.changeToCompletedMissionDTO(memberMission));
     }
